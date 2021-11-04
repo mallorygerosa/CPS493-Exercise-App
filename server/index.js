c866fdfcbd8431ca0c77a5870f917befd3ea0560
MalloryGerosa <mallorygerosa@gmail.com>
2021-11-04 18:34:00 -0400

    Fixed posts and users

--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+console.log(`I hope everything ${process.env.WORKING}`);
+
+const usersController = require('./controllers/users');
+const postsController = require('./controllers/posts');
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app
-.get('/', (req, res) => {
-  res.send('Fitness tracker app')
-})
-.get('/newpaltz', (req, res) => {
-    res.send('Here it is')    
-})
+    .use('/', express.static(path.join(__dirname, '../docs')) )
+
+    .use(express.json())
+    .use('/users', usersController )
+    .use('/posts', postsController)
+
+app
+    // .get('*', (req, res) => res.sendFile(path.join(__dirname, '../docs/index.html')) )
+
+app
+    .use((err, req, res, next)=>{
+        res.status(err.code || 500).send(err);
+    })
 
 app.listen(port, () => {
-  console.log(`Running here: http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
