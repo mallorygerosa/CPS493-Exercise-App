249838c2cb7c60e3e008257e7275cb930b687d9a
MalloryGerosa <mallorygerosa@gmail.com>
2021-12-16 14:42:48 -0500

    Fixing Heroku

--- server/models/mongo.js
+++ server/models/mongo.js
@@ -2,6 +2,7 @@ const { MongoClient } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ttdxr.mongodb.net/myFirstDatabase?retryWrites=true`;
 
+console.log({ uri })
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const isConnected = client.connect();
